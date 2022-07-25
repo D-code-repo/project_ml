7b0d0e37f48f315ff8ffee31fbb44c08b284f30b
D-code-repo <darshand56@gmail.com>
2022-07-25 21:33:47 +0530

    logger and exception

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+import sys
 from flask import Flask
-
+from insurance.logger import logging
+from insurance.exception import InsuranceException
 app=Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 
 def index():
+    try:
+        raise Exception("we are testing custom exception")
+    except Exception as e:
+        insurance = InsuranceException(e,sys)
+        #raise InsuranceException (e,sys) from e
+        logging.info(insurance.error_message)
+        logging.info("We are testing logging module")
     return "Starting Insurance Prediction ML Project"
 
 if __name__=="__main__":

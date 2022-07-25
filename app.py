c690a6c7696a48f5c4678b9a64b02cabe2aec469
D-code-repo <darshand56@gmail.com>
2022-07-25 13:48:57 +0530

    Heroku Deploy

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 @app.route("/", methods=['GET','POST'])
 
 def index():
-    return "Starting insurence prediction ML Project"
+    return "Starting Insurance Prediction ML Project"
 
 if __name__=="__main__":
     app.run(debug=True)

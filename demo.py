e4d959cdc7b6c1da50e25a128cc69a2ca654b94f
D-code-repo <darshand56@gmail.com>
2022-07-28 22:06:57 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,12 +1,15 @@
 from insurance.exception import InsuranceException
 from insurance.pipeline.pipeline import Pipeline
 from insurance.logger import logging
+from insurance.config.configuration import Configuration
 
 
 def main():
     try:
         pipeline = Pipeline()
         pipeline.run_pipeline()
+        #data_validation_config = Configuration().get_data_validation_config()
+        # print(data_validation_config)
     except Exception as e:
         logging.error(f"{e}")
         print(e)
--- insurance/pipeline/pipeline.py
+++ insurance/pipeline/pipeline.py
@@ -6,6 +6,7 @@ from insurance.exception import InsuranceException
 from insurance.entity.artifact_entity import DataIngestionArtifact
 from insurance.entity.config_entity import DataIngestionConfig
 from insurance.component.data_ingestion import DataIngestion
+from insurance.component.data_validation import DataValidation
 
 
 class Pipeline:
@@ -24,8 +25,14 @@ class Pipeline:
         except Exception as e:
             raise InsuranceException(e, sys) from e
 
-    def start_data_validation(self):
-        pass
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataIngestionArtifact:
+        try:
+            data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config(),
+                                             data_ingestion_artifact=data_ingestion_artifact
+                                             )
+            return data_validation.initiate_data_validation()
+        except Exception as e:
+            raise InsuranceException(e, sys) from e
 
     def start_data_transformation(self):
         pass
@@ -43,6 +50,8 @@ class Pipeline:
         try:
             # data ingestion
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact)
 
         except Exception as e:
             raise InsuranceException(e, sys) from e

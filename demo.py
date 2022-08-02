03bdfad698ec9e897a65b34e7fad19a643ecbaf0
D-code-repo <darshand56@gmail.com>
2022-08-02 22:29:29 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,15 +1,28 @@
+import os
 from insurance.exception import InsuranceException
 from insurance.pipeline.pipeline import Pipeline
 from insurance.logger import logging
 from insurance.config.configuration import Configuration
+from insurance.component.data_transformation import DataTransformation
 
 
 def main():
     try:
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
-        #data_validation_config = Configuration().get_data_validation_config()
+        config_path = os.path.join("config", "config.yaml")
+        pipeline = Pipeline(Configuration(config_file_path=config_path))
+        #pipeline = Pipeline()
+        # pipeline.run_pipeline()
+        pipeline.start()
+        logging.info("main function execution completed.")
+        #data_validation_config = Configuration().get_data_transformation_config()
         # print(data_validation_config)
+        #schema_file_path = r"E:\python\project\project_ml\config\schema.yaml"
+        #file_path = r"E:\python\project\project_ml\insurance\artifact\data_ingestion\2022-07-28_21-29-39\ingested_data\train\insurance.csv"
+        # df = DataTransformation.load_data(
+        #   file_path=file_path, schema_file_path=schema_file_path)
+        # print(df.columns)
+        # print(df.dtypes)
+
     except Exception as e:
         logging.error(f"{e}")
         print(e)
--- insurance/logger/__init__.py
+++ insurance/logger/__init__.py
@@ -1,10 +1,16 @@
 from datetime import datetime
 import logging
-from datetime import datetime
 import os
+import pandas as pd
 from pickle import TRUE
+from insurance.constant import get_current_time_stamp
 
 LOG_DIR = "insurance_logs"
+
+
+def get_log_file_name():
+    return f"log_{get_current_time_stamp()}.log"
+
 
 CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
@@ -19,3 +25,20 @@ logging.basicConfig(filename=LOG_FILE_PATH,
                     format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO
                     )
+
+
+def get_log_dataframe(file_path):
+    data = []
+    with open(file_path) as log_file:
+        for line in log_file.readlines():
+            data.append(line.split("^;"))
+
+    log_df = pd.DataFrame(data)
+    columns = ["Time stamp", "Log Level", "line number",
+               "file name", "function name", "message"]
+    log_df.columns = columns
+
+    log_df["log_message"] = log_df['Time stamp'].astype(
+        str) + ":$" + log_df["message"]
+
+    return log_df[["log_message"]]
